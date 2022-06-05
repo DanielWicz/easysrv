387856fe37cc0f5f8b8a1131e947693f9ecf6003
Daniel Wiczew <daniel.wiczew@gmail.com>
2022-06-05 22:05:18 +0200

    Add AutoClipper to clip gradients, when gradient is exploding

--- src/easysrv/utils/mlutils.py
+++ src/easysrv/utils/mlutils.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import tensorflow_probability as tfp
 
 
 def rao_blackwell_ledoit_wolf(cov, N):
@@ -85,3 +86,38 @@ def metric_VAMP2(shift, back):
     eig_sum_sq = 1 + tf.reduce_sum(diag**2)
 
     return eig_sum_sq
+
+
+class AutoClipper:
+    """
+    Source: https://github.com/pseeth/autoclip/blob/master/autoclip_tf.py
+    """
+
+    def __init__(self, clip_percentile, history_size=10000):
+        self.clip_percentile = clip_percentile
+        self.grad_history = tf.Variable(tf.zeros(history_size), trainable=False)
+        self.i = tf.Variable(0, trainable=False)
+        self.history_size = history_size
+
+    def __call__(self, grads_and_vars):
+        grad_norms = [self._get_grad_norm(g) for g, _ in grads_and_vars]
+        total_norm = tf.norm(grad_norms)
+        assign_idx = tf.math.mod(self.i, self.history_size)
+        self.grad_history = self.grad_history[assign_idx].assign(total_norm)
+        self.i = self.i.assign_add(1)
+        clip_value = tfp.stats.percentile(
+            self.grad_history[: self.i], q=self.clip_percentile
+        )
+        return [(tf.clip_by_norm(g, clip_value), v) for g, v in grads_and_vars]
+
+    def _get_grad_norm(self, t, axes=None, name=None):
+        values = tf.convert_to_tensor(
+            t.values if isinstance(t, tf.IndexedSlices) else t, name="t"
+        )
+
+        # Calculate L2-norm, clip elements by ratio of clip_norm to L2-norm
+        l2sum = tf.math.reduce_sum(values * values, axes, keepdims=True)
+        pred = l2sum > 0
+        # Two-tap tf.where trick to bypass NaN gradients
+        l2sum_safe = tf.where(pred, l2sum, tf.ones_like(l2sum))
+        return tf.squeeze(tf.where(pred, tf.math.sqrt(l2sum_safe), l2sum))
