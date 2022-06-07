72be5336150df65b2bb800d488a06f8ee00f96c4
Daniel Wiczew <daniel.wiczew@gmail.com>
2022-06-07 18:07:27 +0200

    Change minimum python version, and add python 3.10 to gitworkflows

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     install_requires=["numpy", "tensorflow", "scipy", "tensorflow_probability"],
     extras_require={
         "dev": ["check-manifest"],
